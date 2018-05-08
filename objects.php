aa98c8ac1d407a0898c40fba0f14ae9a1bd0ebb4
ArthurMagambo <edwin.okumu@strathmore.edu>
2018-05-08 15:43:42 +0300

    classwork

---
+++ objects.php
@@ -0,0 +1,20 @@
+<?php
+class Remember{
+  //attributes
+  private $oop;
+//clonstructor
+  public function __construct(){
+    echo '"Remember"- Mufasa said to Simba in the field';
+  }
+//methods
+public function getOop(){
+  return $this->oop;
+}
+public function setOop($param){
+  $this->oop = $param;
+}
+}
+$newObject = new Remember();
+
+
+?>
